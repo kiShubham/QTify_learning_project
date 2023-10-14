5218dd14d98057c07cda75c193c4ca21df0ab91f
Shubham singh <shubhamshubhank1997@gmail.com>
2023-10-14 16:38:43 +0530

    card component and Api fetching folder

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import { fetchTopAlbums } from "./api/api";
+import Card from "./components/Card/Card";
 
 function App() {
+  const [topAlbumsData, setTopAlbumsData] = useState([]);
+
+  const generateTopAlbumsData = async () => {
+    const data = await fetchTopAlbums();
+    setTopAlbumsData(data);
+  };
+
+  useEffect(() => {
+    generateTopAlbumsData();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <Hero />
+      {topAlbumsData.map((item) => {
+        return <Card data={item} type="albums" />;
+      })}
     </div>
   );
 }
