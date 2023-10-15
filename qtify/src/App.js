6d1000dcecb1cc9aac5619d7bd908195db6987d2
Shubham singh <shubhamshubhank1997@gmail.com>
2023-10-15 16:01:37 +0530

    carosel added

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -3,18 +3,25 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import { fetchTopAlbums } from "./api/api";
+import { fetchNewAlbums } from "./api/api";
 import Section from "./components/Section/Section";
 
 function App() {
   const [topAlbumsData, setTopAlbumsData] = useState([]);
+  const [newAlbumsData, setNewAlbumsData] = useState([]);
 
   const generateTopAlbumsData = async () => {
     const data = await fetchTopAlbums();
     setTopAlbumsData(data);
   };
+  const generateNewAlbumsData = async () => {
+    const data = await fetchNewAlbums();
+    setNewAlbumsData(data);
+  };
 
   useEffect(() => {
     generateTopAlbumsData();
+    generateNewAlbumsData();
   }, []);
 
   return (
@@ -23,6 +30,9 @@ function App() {
       <Hero />
       <div>
         <Section title="Top Albums" data={topAlbumsData} type="albums" />
+      </div>
+      <div>
+        <Section title="New Albums" data={newAlbumsData} type="albums" />
       </div>
     </div>
   );
